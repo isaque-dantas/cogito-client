b81ea4b8b106979b7a489405bc5db821f216f825
isaque-dantas <vicisaque413@gmail.com>
2025-06-30 08:25:52 -0300

    add

--- src/app/pages/course-creation-page/course-creation-page.ts
+++ src/app/pages/course-creation-page/course-creation-page.ts
@@ -1,12 +1,8 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Header} from '../../components/header/header';
-import {FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {CourseFormService} from '../../services/course-form-service';
-import {CourseService} from '../../services/course.service';
-import {Module} from '../../interfaces/module';
+import {ReactiveFormsModule} from '@angular/forms';
 import {CourseForm} from '../../interfaces/course';
-import {AlertService} from '../../services/alert';
-import {HttpErrorResponse} from '@angular/common/http';
+import {CourseFormBasePage} from '../../components/course-form-base-page/course-form-base-page';
 
 @Component({
   selector: 'app-course-creation-page',
@@ -14,78 +10,23 @@ import {HttpErrorResponse} from '@angular/common/http';
     Header,
     ReactiveFormsModule
   ],
-  templateUrl: './course-creation-page.html',
-  styleUrl: './course-creation-page.css'
+  templateUrl: '../../components/course-form-base-page/course-form-base-page.html',
+  styleUrls: [
+    '../../components/course-form-base-page/course-form-base-page.css',
+    './course-creation-page.css'
+  ]
 })
-export class CourseCreationPage implements OnInit {
-  courseService = inject(CourseService)
-  alertService = inject(AlertService)
-  formService = inject(CourseFormService)
-  fb = inject(FormBuilder)
+export class CourseCreationPage extends CourseFormBasePage {
+  override formTitle = 'Adicionar curso'
+  override submitButtonLabel = 'Enviar'
 
-  form = this.fb.group({
-    title: ['', [Validators.required]],
-    modules: this.fb.array([this.formService.moduleGroupFactory()], Validators.min(1))
-  })
-
-
-  get modules() {
-    return this.form.controls.modules as FormArray<FormGroup>;
-  }
-
-  ngOnInit() {
-    this.form.valueChanges.subscribe(() => {
-      console.log(
-        this.formService.getLessonsFromModule(this.modules.at(0)).at(0)!.get('video_link')!.errors
-      )
-    })
-  }
-
-  addModule() {
-    this.modules.push(this.formService.moduleGroupFactory())
-  }
-
-  addLesson(moduleIndex: number) {
-    (this.modules.at(moduleIndex).controls as any).lessons.push(this.formService.lessonGroupFactory())
-  }
-
-  removeModule(moduleIndex: number) {
-    if (this.modules.length == 1) return;
-    this.modules.removeAt(moduleIndex);
-  }
-
-  removeLesson(moduleIndex: number, lessonIndex: number) {
-    console.log("babab")
-    const lessons = (this.modules.at(moduleIndex).controls as any).lessons
-
-    if (lessons.length == 1) return;
-    lessons.removeAt(lessonIndex)
-  }
-
-  onSubmit() {
-    if (this.form.invalid) return;
-
-    let courseForm = this.form.value as CourseForm
-    courseForm = this.formService.replaceVideoLinksByIdsInCourseForm(courseForm)
-
+  override sendFormToServer(courseForm: CourseForm) {
     this.courseService.create(courseForm).subscribe({
       next: (course) => {
         this.alertService.success(`Curso '${course.title}' criado com sucesso.`)
-        console.log(course)
         this.form.reset()
       },
-      error: (error: HttpErrorResponse) => {
-        const messageByStatus: {[status: number]: string} = {
-          0: error.message,
-          401: "Você precisa estar logado(a) para criar um curso.",
-          403: "Você precisa ser coordenador(a) para criar um curso.",
-        }
-
-        let message = messageByStatus[error.status]
-        if (message == undefined) message = messageByStatus[0]
-
-        this.alertService.error(message)
-      },
+      error: this.handleFormError.bind(this),
     })
   }
 }
